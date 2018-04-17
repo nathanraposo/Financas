036e50e8bc9c3759e6c590b01fbab1e6073b0b31
Nathan Raposo <nathanraposo@hotmail.com>
2018-04-17 16:55:20 -0300

    Initial commit

---
+++ app/src/main/java/nathan/com/financas/ui/activity/ListaTransacoesActivity.kt
@@ -0,0 +1,13 @@
+package nathan.com.financas.ui.activity
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+
+class ListaTransacoesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+}
